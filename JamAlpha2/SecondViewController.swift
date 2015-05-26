92d2dcf20b1622d6ece4e45ba0fa20c99211990e
Song Liao <song@x-131-212-236-142.uofm-secure.wireless.umn.edu>
2015-05-26 15:59:22 -0500

    Initial Commit

---
+++ JamAlpha2/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  JamAlpha2
+//
+//  Created by Song Liao on 5/26/15.
+//  Copyright (c) 2015 Song Liao. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
