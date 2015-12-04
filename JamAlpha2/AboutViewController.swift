bfd56768585e578670b8825772c0d3e14c9b3470
Jun Zhou <jun@mystride.co>
2015-12-04 14:05:31 -0600

    add send mail vc

---
+++ JamAlpha2/AboutViewController.swift
@@ -0,0 +1,35 @@
+//
+//  AboutViewController.swift
+//  JamAlpha2
+//
+//  Created by Jun Zhou on 12/4/15.
+//  Copyright © 2015 Song Liao. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- JamAlpha2/SettingsViewController.swift
+++ JamAlpha2/SettingsViewController.swift
@@ -7,24 +7,84 @@
 //
 
 import UIKit
+import MessageUI
 
 class SettingsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    let tableViewContent: [String] = ["Contact Us", "About","Logout"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUpNavigationBar()
+    }
+    
+    func setUpNavigationBar() {
+        self.navigationItem.title = "Setting"
+    }
+    
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return tableViewContent.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("settingscell", forIndexPath: indexPath)
-        cell.textLabel?.text = "Logout"
+        cell.textLabel?.text = tableViewContent[indexPath.item]
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        
-        CoreDataManager.logoutUser()
-
+        if indexPath.item == 0 {
+            self.contactUs()
+        } else if indexPath.item == 1 {
+            let aboutVC: AboutViewController = self.storyboard?.instantiateViewControllerWithIdentifier("aboutVC") as! AboutViewController
+            self.navigationController?.pushViewController(aboutVC, animated: true)
+        }
+        if indexPath.item == 2 {
+            let refreshAlert = UIAlertController(title: "Log Out", message: "Are you sure you want to Log Out?", preferredStyle: UIAlertControllerStyle.Alert)
+            refreshAlert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) in
+                CoreDataManager.logoutUser()
+            }))
+            refreshAlert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.Default, handler: { (action: UIAlertAction!) in
+                self.dismissViewControllerAnimated(false, completion: nil)
+            }))
+            
+        }
         self.navigationController?.popViewControllerAnimated(false)
+    }
+}
+
+extension SettingsViewController: MFMailComposeViewControllerDelegate {
+    func contactUs() {
+        let emailTitle = "Your feed back"
+        let messageBody = "Please tell us what are your suggestions! And if you find bugs, please discribe them with details. Thank you."
+        let toRecipents = ["userfeedback@twistjam.com"]
+        let mc: MFMailComposeViewController = MFMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            
+            mc.mailComposeDelegate = self
+            mc.setSubject(emailTitle)
+            mc.setMessageBody(messageBody, isHTML: false)
+            mc.setToRecipients(toRecipents)
+        
+            self.presentViewController(mc, animated: true, completion: nil)
+        }
+    }
+
+    func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        switch result.rawValue {
+        case MFMailComposeResultCancelled.rawValue:
+            print("Mail cancelled")
+        case MFMailComposeResultSaved.rawValue:
+            print("Mail saved")
+        case MFMailComposeResultSent.rawValue:
+            print("Mail sent")
+        case MFMailComposeResultFailed.rawValue:
+            print("Mail sent failure: \(error!.localizedDescription)")
+        default:
+            break
+        }
+        controller.dismissViewControllerAnimated(true, completion: nil)
     }
 }
