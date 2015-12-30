1e2d74f3922a2eeca6737b9005e903aa85ea4b2e
Jun Zhou <jun@mystride.co>
2015-12-30 14:07:09 -0600

    fixed pan music control view bug

--- JamAlpha2/TabsEditorViewController.swift
+++ JamAlpha2/TabsEditorViewController.swift
@@ -1548,7 +1548,7 @@ class TabsEditorViewController: UIViewController, UICollectionViewDelegateFlowLa
             let translation = sender.translationInView(self.view)
             self.currentBaseButton.center = CGPointMake(self.currentBaseButton.center.x, self.currentBaseButton.center.y)
             sender.setTranslation(CGPointZero, inView: self.view)
-            if self.currentTime >= 0 && self.currentTime <= self.duration {
+            if self.currentTime >= -0.1 && self.currentTime <= self.duration + 0.1 {
                 let timeChange = NSTimeInterval(-translation.x / 10)
                 self.toTime = self.currentTime + timeChange
                 if self.toTime < 0 {
