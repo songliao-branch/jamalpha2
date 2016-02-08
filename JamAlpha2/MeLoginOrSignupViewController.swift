073420bf6b0159f59f553f19cd64d1857b4770b5
Song Liao <liaosong1991@hotmail.com>
2016-02-07 18:39:06 -0600

    show facebook button

--- JamAlpha2/MeLoginOrSignupViewController.swift
+++ JamAlpha2/MeLoginOrSignupViewController.swift
@@ -233,8 +233,8 @@ class MeLoginOrSignupViewController: UIViewController{
         scrollView.addSubview(facebookButton)
         
         //TODO: hide facebook in beta mode
-        facebookButton.hidden = true
-        orLabel.hidden = true
+//        facebookButton.hidden = true
+//        orLabel.hidden = true
         
         //log in screen
         let credentialTextFieldUnderline2 = UIView(frame: CGRect(x: nickNameTextField.frame.origin.x, y: CGRectGetMaxY(emailTextField.frame), width: nickNameTextField.frame.width, height: 1))
